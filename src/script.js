8838e4d568ba8d61fa346958f5afb080b1dad66f
yqa5116 <yqa5116@psu.edu>
2024-09-17 20:29:55 -0400

    /

---
+++ src/script.js
@@ -0,0 +1,56 @@
+var commands = document.getElementById("commands");
+var user_input = document.getElementById("user_input");
+var terminal_outputs = document.getElementById("terminal_outputs");
+var terminal_content = document.getElementById("terminal_content");
+
+function execute(input){
+    let output;
+    input = input.toLowerCase();
+    output = `<div>→ ${input}</div>`;
+    if(!COMMANDS.hasOwnProperty(input)){
+        output += `<div><span class="command">No such command: </span>${input} </div>`;
+    }
+    else{
+        output += `<div>${COMMANDS[input]} </div>`;
+    }
+    
+    terminal_outputs.innerHTML = `${terminal_outputs.innerHTML+output}`;
+    terminal_content.scrollTop = terminal_content.scrollHeight;
+
+    if(input == "github"){
+        setTimeout(() => {window.open("https://github.com/psu-y25", "_blank");}, 2000);
+    }
+}
+
+function key(e){
+    const input = user_input.value;
+
+    if(e.key == "Enter"){
+        execute(input);
+        user_input.value = "";
+        return;
+    }
+
+    user_input.innerHTML = input + e.key;
+}
+
+document.addEventListener("keypress", key);
+
+const COMMANDS = 
+{
+    help:
+        'Supported commands: <span class="command-keyword">"about"</span>, <span class="command-keyword">"experience"</span>, <span class="command-keyword">"education"</span>, <span class="command-keyword">"skills"</span>, <span class="command-keyword">"contact"</span>, <span class="command-keyword">"github"</span>',
+    about:
+        "Hello, my name is Yashas, a Computer Science student at Penn State!",
+    experience:
+        'I have worked on websites, bots on discord, and computer vision software!',
+    education:
+        'I am an undergraduate student looking to go to grad school in the near future.',
+    skills:
+        'I can juggle!',
+    contact:
+        'You can never find me! HAHAHA!',
+    github:
+        'Opening my GitHub page ...'
+    
+}
